31b143790125a29010a01665f6b45d7fd2c9e43a
Sarah Knutson <knutson24sarah@gmail.com>
2016-12-08 00:41:49 +0000

    database access

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,13 +1,13 @@
 var myApp = angular.module("myApp",["firebase"]);
-myApp.controller("tasksController", ["$scope", "$firebaseArray",
-function($scope, $firebaseArray) {
+myApp.controller("tasksController", function($scope, $firebaseArray) {
+  console.log("in tasksController");
   var tasksRef = firebase.database().ref().child("achievements");
+  console.log(tasksRef);
   $scope.tasks = $firebaseArray(tasksRef);
-
+  console.log($scope.tasks);
   var usersRef = firebase.database().ref().child("users");
   $scope.users = $firebaseArray(usersRef);
   
-
  
   $scope.update = function(user) {
      var newuser = {imageURL:user.image,name:user.name,points:0};
@@ -15,5 +15,5 @@ function($scope, $firebaseArray) {
       $scope.users.$add(newuser);
       user.chat = "";
   }
-}
-]);
+
+});
