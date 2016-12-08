93f2b694a95c7ed6488a3ab2f9a9e7edf5fb954e
Sarah Knutson <knutson24sarah@gmail.com>
2016-12-08 05:21:36 +0000

    lots of changes

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,19 +1,23 @@
 var myApp = angular.module("myApp",["firebase"]);
-myApp.controller("tasksController", function($scope, $firebaseArray) {
+myApp.controller("tasksController", ["$scope", "$firebaseArray", "$location",
+function($scope, $firebaseArray, $location) {
   console.log("in tasksController");
-  var tasksRef = firebase.database().ref().child("achievements");
-  console.log(tasksRef);
+  var tasksRef = firebase.database().ref().child("tasks");
   $scope.tasks = $firebaseArray(tasksRef);
-  console.log($scope.tasks);
   var usersRef = firebase.database().ref().child("users");
   $scope.users = $firebaseArray(usersRef);
   
+  $scope.goToAccount = function() {
+    console.log("in app.js goToAccount function");
+    window.location = ('/user');
+  }
  
   $scope.update = function(user) {
-     var newuser = {imageURL:user.image,name:user.name,points:0};
-      console.log(newuser);
-      $scope.users.$add(newuser);
-      user.chat = "";
+    console.log("updating new user");  
+    var newuser = {imageURL:user.photoURL,name:user.displayName,points:0};
+    console.log(newuser);
+    $scope.users.$add(newuser);
   }
 
-});
+}
+]);
